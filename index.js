9a1e549c223493157dbf756c0b4ee77021e45f7d
Nir Yariv <niryariv@gmail.com>
2019-12-08 22:46:11 +0200

    refactor

--- index.js
+++ index.js
@@ -1,123 +1,131 @@
-const API_URL = "https://www.odata.org.il/group/entities/api?name=";
+var svg = d3.select("svg");
+var g 	= svg.append("g");
+var links_class = g.append('g').attr('class', 'links');
+var nodes_class = g.append('g').attr('class', 'nodes');
 
-var current = window.location.search.substring(1);
-if (current == "") current = "mof";
+var width  = svg.attr("width"),
+	height = svg.attr("height");
 
-var svg = d3.select("svg"),
-width = +svg.attr("width"),
-height = +svg.attr("height");
 
-var simulation = d3.forceSimulation()
-.force("link", d3.forceLink().id(function(d) { return d.id; }))
-.force("charge", d3.forceManyBody().strength(-1000))
-.force("center", d3.forceCenter(width / 2, height / 2));
+var nodes = []; // [{id:"nodi"}, {id:"nod"}, {id:1}, {id:121}];
+var links = []; //[{ source: 'nod', target: 'nodi' }
+			// , { source: 'nod', target: 121 }
+			// , { source: 'nod', target: 1 }];
 
-d3.json(API_URL + current, function(error, odata) {
-	if (error) throw error;
-	
-	var graph = {nodes:[], links:[]};
-	
-	graph.nodes = odata.related_groups;
-	
-	
-	// set center element
-	graph.nodes.unshift(odata.group);
-	
-	graph.nodes.forEach(function(d, i){
-		d.id = i;
-		d.text = d.display_name;
-		if (d.name != odata.group.name) // skip the center group when it appears in the results
-		graph.links.push({source: odata.group.id, target:i});
+var center_node;
+
+var simulation = d3.forceSimulation(nodes)
+	.force('charge', d3.forceManyBody().strength(-20))
+	.force('center', d3.forceCenter(width / 2, height / 2))
+	.force('collision', d3.forceCollide().radius(30))
+	.force('link', d3.forceLink().id(function (d) { return d.id }).links(links))
+	.on('tick', render);
+
+
+//TODO:
+
+// - load nodes from API (use add_node())
+// - convert to d3 format
+// - render()
+
+// why isn't this part of remove_node? because we actually want to keep the original node. 
+// remove_node is really just a helper
+function remove_children_nodes(node){
+	var children = nodes.filter(function(n){ return n.parent == node.id});
+	return children.length == 0 ? false : children.forEach(function(n) { remove_children_nodes(n) ; remove_node(n)});
+}
+
+function remove_node(node){
+	links = _.reject(links, function(l) {return (l.source == node || l.target == node) });
+	nodes = _.reject(nodes, function(n) {return (n == node || n.parent == node) })
+	simulation.nodes(nodes);
+	simulation.force('link', d3.forceLink().id(function (d) { return d.id }).links(links))
+	simulation.restart();
+}
+
+function center_on_node(node){
+	center_node = node;
+
+	var dcx = (width / 2 - node.x);
+	var dcy = (height / 2 - node.y);
+	g.transition().attr("transform", "translate(" + dcx + "," + dcy + ")");
+
+	simulation.alpha(1).restart();
+
+	return true;
+}
+
+function toggle_node(node){
+	children = _.where(nodes, {parent:node.id});
+	return children.length > 0 ? remove_children_nodes(node) : load_nodes_under(node.id);
+}
+
+function node_exists(node){
+	return (_.findWhere(nodes, {id:node.id}) ? true : false);
+}
+
+function add_node(new_node, parent=false){
+	// debugger
+	if (node_exists(new_node)) return false;
+
+	nodes.push(new_node);
+	if (parent) links.push({ source: parent.id, target: new_node.id});
+
+	simulation.nodes(nodes);
+	simulation.force('link', d3.forceLink().id(function (d) { return d.id }).links(links))
+	simulation.alpha(1).restart();
+}
+
+function render() {
+	var n = d3.select('.nodes')
+		.selectAll('text')
+		.data(nodes)
+
+	n.enter()
+		.append('text')
+		.merge(n)
+		.on('click', function (d) { return center_on_node(d) }) 
+		.on('click', function (d) { return toggle_node(d) }) 
+		.text(function (d) { return d.display_name })
+		.style('font-size', function (d) { return d == center_node ? '20px' : '10px' })
+		.attr('x', function (d) { return d.x; })
+		.attr('y', function (d) { return d.y; });
+
+	n.exit().remove()
+
+	var l = d3.select('.links')
+		.selectAll('line')
+		.data(links)
+
+	l.enter()
+		.append('line')
+		.merge(l)
+		.attr('x1', function (d) { return d.source.x })
+		.attr('y1', function (d) { return d.source.y })
+		.attr('x2', function (d) { return d.target.x })
+		.attr('y2', function (d) { return d.target.y })
+
+	l.exit().remove()
+}
+
+function api_url(param){
+	return "https://www.odata.org.il/group/entities/api?name=" + param;
+}
+
+function load_nodes_under(node_id){
+	console.log("load_nodes_under:", node_id);
+	d3.json(api_url(node_id), function(odata){
+		odata.group.id = odata.group.name;
+		add_node(odata.group, center_node ? center_node : false);
+		center_node = odata.group;
+
+		odata.related_groups.forEach(function (g) {
+			g.id = g.name;
+			add_node(g, center_node);
+		});
 	});
-	
-	console.log(graph); 
-	
-	var link = svg.append("g")
-	.style("stroke", "#aaa")
-	.selectAll("line")
-	.data(graph.links)
-	.enter().append("line");
-	
-	// var node = svg.append("g")
-	// .attr("class", "nodes")
-	// .selectAll("circle")
-	// .data(graph.nodes)
-	// .enter().append("circle")
-	// .attr("r", 10)
-	// .call(d3.drag()
-	// .on("start", dragstarted)
-	// .on("drag", dragged)
-	// .on("end", dragended));
-	
-	var label = svg.append("g")
-		.attr("class", "labels")
-		.selectAll("text")
-		.data(graph.nodes)
-		.enter()
-			.append("a")
-			.attr("xlink:href", function (d) { 
-					return window.location.href.split('?')[0]+"?"+d.name 
-			})
-				.append("text")
-				.attr("class", "label")
-				.attr("text-anchor", "middle")				
-				.text(function(d) { return d.display_name });
-	
-	// var label = new d3plus.TextBox()
-	// 	.data(graph.nodes)
-	// 	.fontSize(10)
-	// 	.width(60)
-	// 	// .x(function (d, i) { return d.x; })
-	// 	// .y(function (d, i) { return d.y; })
-	// 	.render();
-	
-	simulation
-	.nodes(graph.nodes)
-	.on("tick", ticked);
-	
-	simulation.force("link")
-	.links(graph.links);
-	
-	function ticked() {
-		link
-		.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; })
-		.attr("stroke", "#eee")
-		.attr("stroke-width", function (d) { return d.target.count + "px"});
-		
-		// node
-		// .attr("r", 3)
-		// .style("fill", "#e38d31")
-		// .style("stroke", "#d9d9d9")
-		// .style("stroke-width", "1px")
-		// .attr("cx", function (d) { return d.x; })
-		// .attr("cy", function(d) { return d.y; });
-		
-		// label
-		// 	// .x(function (d) { return d.x; })
-		// 	// .y(function (d) { return d.y; })
-		// 	.render();
-		label
-		.attr("x", function(d) { return d.x; })
-		.attr("y", function (d) { return d.y; })
-			.style("font-size", function (d) { return (d.name == odata.group.name ? "20px" : "10px")});
+}
 
-		
-	};
-	
-	function dragstarted(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-		simulation.fix(d);
-	}
-	
-	function dragged(d) {
-		simulation.fix(d, d3.event.x, d3.event.y);
-	}
-	
-	function dragended(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
-		simulation.unfix(d);
-	}
-});
+node_id = window.location.search.substring(1) || "mof";
+load_nodes_under(node_id);
+
